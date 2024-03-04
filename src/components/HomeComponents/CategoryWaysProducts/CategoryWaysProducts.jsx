534c709f081a06fe15729291af223abdf8a11e56
nasim0994 <nasimuddin0994@gmail.com>
2024-03-04 10:17:42 +0600

    a

--- src/components/HomeComponents/CategoryWaysProducts/CategoryWaysProducts.jsx
+++ src/components/HomeComponents/CategoryWaysProducts/CategoryWaysProducts.jsx
@@ -1,26 +1,28 @@
 import { useGetCategoriesQuery } from "../../../Redux/category/categoryApi";
+import CWP from "../../Skeleton/CWP/CWP";
 import ProductsSection from "../productsSection/productsSection";
-import CategoryWaysProduct from "../../Skeleton/CategoryWaysProduct/CategoryWaysProduct";
-
 
 export default function CategoryWaysProducts() {
-  const { data: categories, isLoading,isError,error } = useGetCategoriesQuery();
+  const {
+    data: categories,
+    isLoading,
+    isError,
+    error,
+  } = useGetCategoriesQuery();
 
   let content = null;
 
   if (isLoading) {
-    content = <CategoryWaysProduct/>
+    content = <CWP />;
   }
   if (!isLoading && isError) {
-    content=   <p>{error.error || "something went wrong"}</p>;
+    content = <p>{error.error || "something went wrong"}</p>;
   }
   if (!isLoading && !isError && categories?.data?.length > 0) {
-    content = categories?.data?.map((category) => <ProductsSection key={category?._id} category={category}/>)
+    content = categories?.data?.map((category) => (
+      <ProductsSection key={category?._id} category={category} />
+    ));
   }
 
-  return (
-    <>
-      {content}
-    </>
-  );
+  return <>{content}</>;
 }
